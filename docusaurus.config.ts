4442f479436396a1a93ae2921257f531578eefd8
Bryant KAMAN <firmin.kaman@arondor.com>
2025-04-23 15:58:52 +0200

    UPD: update deploy file for add github-pages

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
 
   organizationName: 'firmin-kaman',
   projectName: 'Uxodocs',
-  deploymentBranch: 'docusaurus',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
